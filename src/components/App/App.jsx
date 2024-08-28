a622dd6caee3aac0a9bb95ef42acb878310cfbac
Hotsyn <85835454+SandraHotsyn@users.noreply.github.com>
2024-08-28 15:13:53 +0300

    add contact

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { nanoid } from "nanoid";
 import ContactList from "../ContactList/ContactList";
 import SearchBox from "../SearchBox/SearchBox";
+import ContactForm from "../ContactForm/ContactForm";
 import "./App.module.css";
 
 export default function App() {
-  const [contacts] = useState([
+  const [contacts, setContacts] = useState([
     { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
     { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
     { id: "id-3", name: "Eden Clements", number: "645-17-79" },
@@ -17,6 +19,15 @@ export default function App() {
     setFilter(event.target.value);
   };
 
+  const addContact = (name, number) => {
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    setContacts((prevContacts) => [...prevContacts, newContact]);
+  };
+
   const filteredContacts = contacts.filter((contact) =>
     contact.name.toLowerCase().includes(filter.toLowerCase())
   );
@@ -24,6 +35,7 @@ export default function App() {
   return (
     <div>
       <h1>Contact List</h1>
+      <ContactForm onSubmit={addContact} />
       <SearchBox filter={filter} onFilterChange={handleFilterChange} />
       <ContactList contacts={filteredContacts} />
     </div>
